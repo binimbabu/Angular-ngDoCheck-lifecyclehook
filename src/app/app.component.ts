59745c381f89c2f29eed7395c2cb9e0c2e103b27
Bini Babu <bini.babu@dbizsolution.com>
2025-07-06 18:34:33 +0530

    ngDoCheck lifecyclehook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ngDoCheck';
+export class AppComponent implements DoCheck {
+  nameInput: any;
+  previousName = '';
+  employee: any = {
+    name: 'Bini',
+    age: 24,
+  };
+  ngDoCheck() {
+    if (this.employee.name !== this.previousName) {
+      this.previousName = this.employee.name;
+    }
+  }
+  changeName(name: any) {
+    this.employee.name = name;
+  }
 }
